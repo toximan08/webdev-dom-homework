a7053469e5cf4adcf293a7d71fd57b87ee799464
Fedor <toximan_08@mail.ru>
2023-04-08 13:50:42 +0300

    hw 2.10 bug fixes

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const commentTextElement = document.getElementById("comment-text");
 const nameInputElement = document.getElementById("name-input");
 const commentTxtareaElement = document.getElementById("comment-txtarea");
 const mainForm = document.querySelector(".add-form");
+let quote = "";
 
 // Массив комментариев
 
@@ -19,6 +20,7 @@ const comments = [
     counter: "3",
     isLiked: true,
     isEdit: false,
+    isReplied: false,
   },
   {
     name: "Варвара Н.",
@@ -27,6 +29,7 @@ const comments = [
     counter: "75",
     isLiked: true,
     isEdit: false,
+    isReplied: false,
   },
 ];
 
@@ -45,7 +48,8 @@ const initLikeButtonsListeners = () => {
   const likeButtonsElements = document.querySelectorAll(".like-button");
 
   for (const likeButtonElement of likeButtonsElements) {
-    likeButtonElement.addEventListener("click", () => {
+    likeButtonElement.addEventListener("click", (event) => {
+      event.stopPropagation();
       const index = likeButtonElement.dataset.index;
 
       if (comments[index].isLiked === true) {
@@ -60,23 +64,44 @@ const initLikeButtonsListeners = () => {
   }
 };
 
-// Функция редактирования комментариев work in progress
+// Функция ответа на комментарии work in progress (задание со звездочкой)
 
-// const initEditButtonsListeners = () => {
-//   const editButtonsElements = document.querySelectorAll(".edit-button");
+const replyListeners = () => {
+  const commentTextElements = document.querySelectorAll(".comment-text");
 
-//   for (const editButtonElement of editButtonsElements) {
-//     editButtonElement.addEventListener("click", () => {
-//       const index = editButtonElement.dataset.index;
+  for (const commentTextElement of commentTextElements) {
+    commentTextElement.addEventListener("click", (event) => {
+      event.stopPropagation();
+      const index = commentTextElement.dataset.index;
+      commentTxtareaElement.value = `> ${comments[index].text} \n ${comments[index].name}, `;
+      quote = `QUOTE_BEGIN ${comments[index].name} \n ${comments[index].text} QUOTE_END`;
+      commentTextElement.setAttribute("readonly", true);
+      if (comments[index].isReplied) {
+        renderComments();
+      }
+    });
+  }
+};
 
-//       if (comments[index].isEdit === true) {
-//         comments[index].isEdit = false;
-//       } else if (comments[index].isEdit === false) {
-//         comments[index].isEdit = true;
-//       }
-//     });
-//   }
-// };
+// Функция редактирования комментариев
+
+const initEditButtonsListeners = () => {
+  const editButtonsElements = document.querySelectorAll(".edit-button");
+
+  for (const editButtonElement of editButtonsElements) {
+    editButtonElement.addEventListener("click", () => {
+      const index = editButtonElement.dataset.index;
+      const editTextArea = document.querySelector(".area-text");
+      if (comments[index].isEdit === true) {
+        comments[index].isEdit = false;
+        comments[index].text = editTextArea.value;
+      } else if (comments[index].isEdit === false) {
+        comments[index].isEdit = true;
+      }
+      renderComments();
+    });
+  }
+};
 
 // Рендер-функция
 
@@ -92,12 +117,21 @@ const renderComments = () => {
             ${
               comment.isEdit
                 ? `<textarea class ="area-text">${comment.text}</textarea>`
-                : `<div class ="comment-text">${comment.text}
-            </div>`
+                : `<div data-index="${index}" class ="comment-text">
+                ${
+                  comment.isReplied
+                    ? `<div class="quote">${quote
+                        .replaceAll("QUOTE_BEGIN", "<div class='quote'>")
+                        .replaceAll("QUOTE_END", "</div>")}</div>`
+                    : ``
+                }
+            ${comment.isReplied ? "" : comment.text}`
             }
           </div>
           <div class="comment-footer">
-            <button data-index="${index}" class="add-form-button edit-button">Редактировать</button>
+            <button data-index="${index}" class="add-form-button edit-button">${
+        comment.isEdit ? `Сохранить` : `Редактировать`
+      }</button>
             <div class="likes">
               <span class="likes-counter">${comment.counter}</span>
               <button data-index="${index}" class="${
@@ -111,6 +145,8 @@ const renderComments = () => {
   listElement.innerHTML = commentsHtml;
 
   initLikeButtonsListeners();
+  initEditButtonsListeners();
+  replyListeners();
 };
 
 renderComments();
@@ -139,14 +175,43 @@ window.addEventListener("input", () => {
 // Функция добавления нового комментария
 
 addButtonElement.addEventListener("click", () => {
-  comments.push({
-    name: nameInputElement.value,
-    date: date,
-    text: commentTxtareaElement.value,
-    counter: 0,
-    isLiked: false,
-    isEdit: false,
-  });
+  if (quote !== "") {
+    comments.push({
+      name: nameInputElement.value
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("&", "&amp;")
+        .replaceAll('"', "&quot;"),
+      date: date,
+      text: commentTxtareaElement.value
+        .replaceAll("<", "&lt;")
+        .replaceAll("&", "&amp;")
+        .replaceAll('"', "&quot;"),
+      counter: 0,
+      isLiked: false,
+      isEdit: false,
+      isReplied: true,
+    });
+  } else {
+    comments.push({
+      name: nameInputElement.value
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("&", "&amp;")
+        .replaceAll('"', "&quot;"),
+      date: date,
+      text: commentTxtareaElement.value
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll("&", "&amp;")
+        .replaceAll('"', "&quot;")
+        .replaceAll('"', "&quot;"),
+      counter: 0,
+      isLiked: false,
+      isEdit: false,
+      isReplied: false,
+    });
+  }
 
   renderComments();
   delValue();
